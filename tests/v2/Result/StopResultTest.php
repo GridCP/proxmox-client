5f2d6c269a228c8f1e3bfdc826a01f936754719d
Guillermo Lengemann Garcés <guillermo.lengemann@gmail.com>
2026-03-12 11:45:46 -0500

    Add unit tests for `StopResult` in `StopResultTest` covering successful response scenarios

---
+++ tests/v2/Result/StopResultTest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GridCP\Proxmox\Tests\Result;
+
+use GridCP\Proxmox\Result\ResultConverter;
+use GridCP\Proxmox\Result\StopResult;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\StreamInterface;
+
+class StopResultTest extends TestCase
+{
+    public function testOnOkResponse(): void
+    {
+        $converter = new ResultConverter();
+
+        $json = json_encode([
+            'data' => 'UPID:ns2202:0010EE9E:00921A9C:69B2ED0F:qmstop:100:root@pam!gridcp:',
+        ], JSON_THROW_ON_ERROR);
+
+        $response = $this->createMock(ResponseInterface::class);
+        $stream = $this->createMock(StreamInterface::class);
+        $stream->method('getContents')->willReturn($json);
+
+        $response->method('getStatusCode')->willReturn(200);
+        $response->method('getReasonPhrase')->willReturn('OK');
+        $response->method('getBody')->willReturn($stream);
+
+        $result = $converter->convert($response, StopResult::class);
+
+        $this->assertInstanceOf(StopResult::class, $result);
+        $this->assertSame(
+            'UPID:ns2202:0010EE9E:00921A9C:69B2ED0F:qmstop:100:root@pam!gridcp:',
+            $result->upid,
+        );
+    }
+}
