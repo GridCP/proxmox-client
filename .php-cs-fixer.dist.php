2828be69566d0b7f68e55c2148bd92a282b82e88
Guillermo Lengemann Garcés <guillermo.lengemann@gmail.com>
2026-03-24 12:16:31 -0500

    Refactor `.php-cs-fixer.dist.php`: simplify `Finder` and `Config` instantiation.

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,11 +1,11 @@
 <?php
 
-$finder = (new PhpCsFixer\Finder())
+$finder = new PhpCsFixer\Finder()
     ->in(__DIR__ . '/src')
     ->exclude('var')
     ->exclude('vendor');
 
-return (new PhpCsFixer\Config())
+return new PhpCsFixer\Config()
     ->setRiskyAllowed(true)
     ->setRules([
         '@Symfony' => true,
